d13dd40d87696704e520b82aee42b465ffef1f05
Oscar Sanchez M <skrsm_12@hotmail.com>
2024-05-20 20:59:53 -0600

    se agrega libreria Gson y se crea la conexion con la API

--- src/com/alura/challenge/conversor/Principal.java
+++ src/com/alura/challenge/conversor/Principal.java
@@ -1,7 +1,41 @@
 package com.alura.challenge.conversor;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Principal {
     public static void main(String[] args) {
+
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
+
+        String apiKey = "22652200d377ea7e3d5df5fd";
+        String url = "https://v6.exchangerate-api.com/v6/" + apiKey + "/latest/USD";
+
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .build();
+
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            String json = response.body();
+
+            System.out.println(json);
+        } catch (IOException | InterruptedException e) {
+            e.getMessage();
+        }
 
     }
 }
